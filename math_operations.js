e7c10bfe5e53b47af9e740d1aa83b976b7abbe4d
mazharul77 <mazharul77.cse@gmail.com>
2026-01-06 03:21:33 +0600

    prototypes gaming with number objects

--- math_operations.js
+++ math_operations.js
@@ -1,2 +1,29 @@
 dynamic_year = 2026
 console.log(`Node at Reaction to JS in ${dynamic_year}`)
+// [constructor(Number), toExponential, toFixed, toLocaleString, toPricision, toString, valueOf, Object]
+
+const readline = require("readline")
+console.log(`readline:${readline} and the type: ${typeof readline}`)
+
+const rd_interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+// console.log("rd_interface",rd_interface)
+
+
+const band_score_standanrd = 9
+console.log(`Band Score Standard:${band_score_standanrd} and Type is:${typeof band_score_standanrd}`)
+
+rd_interface.question(`What's Expected Salary in ${dynamic_year}?:`, (input) => {
+    const numEntered = Number(input)
+    console.log("Ow! You Wanted:", numEntered.toLocaleString("en-IN")) // counting convenience  as local format passed
+    rd_interface.close();
+})
+
+// Number as object
+rd_interface.question("Type a number", (input) => {
+    let age = Number(input)
+});
+// const earningExpectation = new Number(prompt("Enter Your Expected Earning digits in ${dynamic_year}"))
+// console.log(`Expected Earning: ${earningExpectation}`)
