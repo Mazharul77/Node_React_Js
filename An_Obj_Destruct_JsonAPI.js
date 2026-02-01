f4701e124e3d235963c97777079329b48f9189d1
mazharul77 <mazharul77.cse@gmail.com>
2026-02-02 00:54:22 +0600

    Object , Array destructuring with json API

---
+++ An_Obj_Destruct_JsonAPI.js
@@ -0,0 +1,30 @@
+// Object Destructuring with JSON API or Array Destructuring:
+
+const invoiceData = {
+    'invoiceId': 'INV-PI00101',
+    'invName': 'Customer Invoice',
+    'amountTotal': 3500,
+    'currecncy': 'USD',
+    'items': [
+        {
+            'freelanceServId': 'FS001',
+            'description': 'ERP/Ecommerce Solution',
+            'qty': 3,
+            'unitPrice': '$500',
+        },
+        
+    ],
+
+}
+// Destructuring the invoiceData object
+const { invoiceId,amountTotal: amount, items } = invoiceData;
+console.log("The Invoice Id:", invoiceId, "and the Item's unit price:", items[0].unitPrice,
+    `\n with Total Amount: $${amount}`
+);
+
+// Array Destructuring
+const currencies = ['USD', 'EUR', 'GBP', 'INR', 'AUD'];
+const [firstCurrency, secondCurrency, , fourthCurrency] = currencies;
+console.log("First Currency:", firstCurrency);
+console.log("Second Currency:", secondCurrency);
+console.log("Fourth Currency:", fourthCurrency);
