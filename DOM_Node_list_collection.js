82340b3a9dd4460a6f3f5f8b27215259c6907455
mazharul77 <mazharul77.cse@gmail.com>
2026-03-13 20:14:30 +0600

    create, edit/update, delete operations/manipulations in DOM are performed

---
+++ DOM_Node_list_collection.js
@@ -0,0 +1,21 @@
+console.log("DOM Node List Management")
+// console.log('Document:-', document)
+// console.log('Window:-', window)
+
+/* document window object are global objects in the web browser environment.
+They provide access to the web page and its properties, methods, and events.
+ The document object represents the HTML document loaded in the browser,
+ 
+ while the window object represents the browser window itself.
+  Both objects are essential for manipulating and interacting with web pages using JavaScript.
+*/
+// console.log('Document Window Objecet:', document.window) // will work in web browser but not in node js environment
+// console.log('Location:-', document.location)
+// console.log('Base URI:-', document.baeURI)
+// console.log('Document Links:', document.links)
+
+// - To get all the elements of the document with the tag name "p" and store them in a variable called "paragraphs"
+//[in web browser]
+
+//  let paragraphs = document.getElementsByTagName("p")
+// console.log("All the paragraphs in the document:-", paragraphs)
