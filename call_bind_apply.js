9e7a1f379d68febd7d9bf3633fef322a49eef256
mazharul77 <mazharul77.cse@gmail.com>
2026-05-29 19:26:00 +0600

    bind at class constructor's data handler is implemented

--- call_bind_apply.js
+++ call_bind_apply.js
@@ -1,4 +1,4 @@
-// call function with call method along with arguments
+/* call mechanism with call method along with arguments */
 
 // setting properties
 function pupilIdentify(studentId, studentName) {
@@ -10,12 +10,35 @@ function pupilIdentify(studentId, studentName) {
 // ceating an object
 function CreateStudent(id, name, email, section) {
     // calling the function with 'call' method and passing the arguments with 'this' keyword to set the propertes in the current object
-    pupilIdentify.call(this,id, name);
+    pupilIdentify.call(this, id, name);
 
     this.email = email
     this.section = section
 }
 
-const studentG = new CreateStudent(1425, 'Mazharul Islam Bhuiyan',  'mazharul15 - 1425@diu.edu.bd', 'G');
+const studentG = new CreateStudent(1425, 'Mazharul Islam Bhuiyan', 'mazharul15 - 1425@diu.edu.bd', 'G');
 console.log(studentG);
 
+/*
+bind mechanism with bind method:
+*/
+
+class Binder {
+    constructor() {
+        this.name = 'Mazharul Islam Bhuiyan'
+        this.request_url = 'https://www.github.com/mazharul77'
+        this.designation = 'Software Engineer'
+
+        // on click bind method to the button andd passing the arguments to the function
+        document.querySelector('.bind_now').addEventListener('click', this.manageRequest.bind(this, 'Requesting for a job'))
+    }
+    manageRequest(message) {
+        console.log(`The Binder has successfully bound to pass the consturctor's data at manage function!`);
+
+        console.log(`The binding message: ${message}`);
+        console.log(`The Request for binder: ${this.request_url}`);
+
+    }
+}
+
+const boundObj = new Binder();
